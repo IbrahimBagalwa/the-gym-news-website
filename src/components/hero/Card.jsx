3aa5455039b90a9aac71fe815180986be9eb8d38
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-03-27 14:06:48 +0200

    feat: update hero section

--- src/components/hero/Card.jsx
+++ src/components/hero/Card.jsx
@@ -1,10 +1,10 @@
 import testImage from "/images/testImage.jpg";
 const Card = () => {
   return (
-    <div className="flex text-white gap-2 w-[500px]">
-      <img src={testImage} alt="test-image" />
+    <div className={`flex  text-white gap-2 px-10 z-50 hover-group`}>
+      <img src={testImage} alt="test-image" className="cursor-pointer" />
       <div>
-        <p>
+        <p className="hover:underline cursor-pointer">
           Green MEP Bütikofer: Xi visit to Moscow indicates desire for new world
           order
         </p>
--- src/components/hero/CardContainer.jsx
+++ src/components/hero/CardContainer.jsx
@@ -3,9 +3,9 @@ import HeroHeader from "./HeroHeader";
 
 const CardContainer = () => {
   return (
-    <section className="absolute -bottom-[42rem]">
+    <section className="absolute -bottom-[42rem] z-50">
       <HeroHeader />
-      <div className="w-full bg-[#323C45] flex items-center justify-center py-6 gap-8">
+      <div className=" bg-[#323C45] md:flex items-center justify-center py-6 gap-8">
         <Card />
         <Card />
       </div>
