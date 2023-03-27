4ae5313b1ee5145bc2d3aa1e98d7a15364f86259
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-03-27 14:11:09 +0200

    feat: update wrapper page

--- src/components/wrapper/WrapperContent.jsx
+++ src/components/wrapper/WrapperContent.jsx
@@ -1,6 +1,6 @@
-const WrapperContent = ({ children, className }) => {
+const WrapperContent = ({ children, styles }) => {
   return (
-    <section className={` mx-auto max-w-[1140px] relative ${className}`}>
+    <section className={` mx-auto max-w-[1140px] relative ${styles}`}>
       {children}
     </section>
   );
