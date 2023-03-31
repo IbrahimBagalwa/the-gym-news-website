bc8cb79249f63a88d0c7387194a4dcb625d26900
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-03-31 19:35:48 +0200

    feat: update

--- src/components/news/index.js
+++ src/components/news/index.js
@@ -1,2 +1,28 @@
 import News from "./News";
 export { News };
+const country = [
+  {
+    name: "Rwanda",
+    location: {
+      street: "20 main street",
+      timezone: {
+        offset: "+9:00",
+      },
+    },
+  },
+  {
+    name: "Burundi",
+    location: {
+      street: "78 mirko street",
+    },
+  },
+  {
+    name: "DRC",
+    location: {
+      street: "47160 kin street",
+      timezone: {
+        offset: "+2:00",
+      },
+    },
+  },
+];
