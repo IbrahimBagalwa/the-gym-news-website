035be596bff570b22419bbac2e892c697df1e74b
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-04-05 14:25:52 +0200

    feat update live component

--- src/components/hero/LiveScreen.jsx
+++ src/components/hero/LiveScreen.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LiveScreen = () => {
   return (
-    <div className="video-responsive">
+    <div className="overflow-hidden pb-[56.25%] relative h-0">
       <iframe
         width="1280"
         height="720"
@@ -10,6 +10,7 @@ const LiveScreen = () => {
         title="🔴 Al Jazeera English | Live"
         frameborder="0"
         allowfullscreen
+        className="let-0 top-0 h-full w-full absolute"
       />
     </div>
   );
